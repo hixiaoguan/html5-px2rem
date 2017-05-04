e63111e45a195a8c36e19f130f06fbfe8cc1235f
guanshibin <guanshibin@qing5.com>
2017-05-05 01:11:18 +0800

    first commit

---
+++ html5-px2rem.js
@@ -0,0 +1,17 @@
+/*
+* 痛点：HTML5页面布局单位由px转换rem解决方案
+* 作者：jonyguan [309678100@qq.com]
+* 下载: git clone https://github.com/hixiaoguan/html5-px2rem.git
+*/
+(function (doc, win) {
+    var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = doc.body.clientWidth;
+            if (!clientWidth) return;
+            var theHtml = doc.getElementsByTagName("html");
+            theHtml[0].style.fontSize = 20 * (clientWidth / 320) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
