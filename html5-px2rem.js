c6c78bb9c254daeb5f7ee41299b1d08c8b9c8eda
guanshibin <guanshibin@qing5.com>
2017-05-05 13:14:36 +0800

    +px-em-rem-percent-points.png

--- html5-px2rem.js
+++ html5-px2rem.js
@@ -2,6 +2,7 @@
 * 痛点：HTML5页面布局单位由px转换rem解决方案
 * 作者：jonyguan [309678100@qq.com]
 * 下载: git clone https://github.com/hixiaoguan/html5-px2rem.git
+* 理解：px-em-rem-percent-points.png 可以看看这个图是rem和其他单位之间的关系
 */
 (function (doc, win) {
     var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
