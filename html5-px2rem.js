e20023a193ae7bdb928f1b89578ac70eb830a31d
guanshibin <guanshibin@qing5.com>
2017-05-08 15:56:47 +0800

    调整设置html字体方案，页面宽度320px时，页面根元素HTML字体设置为320/10=32px

--- html5-px2rem.js
+++ html5-px2rem.js
@@ -3,14 +3,16 @@
 * 作者：jonyguan [309678100@qq.com]
 * 下载: git clone https://github.com/hixiaoguan/html5-px2rem.git
 * 理解：px-em-rem-percent-points.png 可以看看这个图是rem和其他单位之间的关系
+* pieces: 把屏幕10等分，我们需要根据实际页面尺寸除10得到页面根元素html字体大小font-size，比如320屏html字体设置为32px
 */
 (function (doc, win) {
+    var pieces = 10;
     var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
             var clientWidth = doc.body.clientWidth;
             if (!clientWidth) return;
             var theHtml = doc.getElementsByTagName("html");
-            theHtml[0].style.fontSize = 20 * (clientWidth / 320) + 'px';
+            theHtml[0].style.fontSize = clientWidth / pieces + 'px';
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
